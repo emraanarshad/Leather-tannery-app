9f4f9af26920cd1e45813a1b94f97a98f0f0e7cf
emraanarshad <emraanarshad710@gmail.com>
2026-06-01 12:14:26 +0500

    Add service worker for asset caching
    Implement a service worker for caching assets.

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = 'tannery-v1';
+const ASSETS = [
+  'index.html',
+  'manifest.json',
+  'logo.png'
+];
+
+// Install Service Worker
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => {
+      return cache.addAll(ASSETS);
+    })
+  );
+});
+
+// Fetch Assets
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
