ab1763fa0bbb132d7cb6f222aa76e88c5527e694
emraanarshad <emraanarshad710@gmail.com>
2026-07-10 17:13:17 +0500

    Upgrade service worker to cache v3 and enhance activation
    Updated service worker to use cache version v3 and improved cache management during activation.

--- sw.js
+++ sw.js
@@ -1,10 +1,35 @@
-const cacheName = 'tannery-app-v2';
-const assets = ['/', '/index.html', '/index.html'];
+const cacheName = 'tannery-app-v3';
+const assets = ['./', './index.html'];
 
+// Nayi files ko install karna
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+    e.waitUntil(
+        caches.open(cacheName).then((cache) => {
+            return cache.addAll(assets);
+        })
+    );
 });
 
+// Puraani cache ko khudraat delete karna (Missing Code)
+self.addEventListener('activate', (e) => {
+    e.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(
+                keys.map((key) => {
+                    if (key !== cacheName) {
+                        return caches.delete(key);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Offline data serve karna
 self.addEventListener('fetch', (e) => {
-    e.respondWith(caches.match(e.request).then((res) => res || fetch(e.request)));
+    e.respondWith(
+        caches.match(e.request).then((res) => {
+            return res || fetch(e.request);
+        })
+    );
 });
