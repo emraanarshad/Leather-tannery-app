1d3121df747535372870820375fd7e39086817d0
emraanarshad <emraanarshad710@gmail.com>
2026-07-10 22:41:06 +0500

    Add external assets to service worker cache

--- sw.js
+++ sw.js
@@ -1,12 +1,18 @@
 const cacheName = 'tannery-v4';
-const assets = ['./', './index.html'];
+const assets = [
+  './',
+  './index.html',
+  'https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css',
+  'https://www.gstatic.com/firebasejs/9.6.0/firebase-app-compat.js',
+  'https://www.gstatic.com/firebasejs/9.6.0/firebase-database-compat.js'
+];
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+  e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
 });
 
 self.addEventListener('fetch', (e) => {
-    e.respondWith(
-        caches.match(e.request).then((res) => res || fetch(e.request))
-    );
+  e.respondWith(
+    caches.match(e.request).then((res) => res || fetch(e.request))
+  );
 });
