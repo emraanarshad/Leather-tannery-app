88ffdb14af19845179381f858cf0ae605228a99c
emraanarshad <emraanarshad710@gmail.com>
2026-07-10 18:45:09 +0500

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,12 @@
+const cacheName = 'tannery-v4';
+const assets = ['./', './index.html'];
+
+self.addEventListener('install', (e) => {
+    e.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener('fetch', (e) => {
+    e.respondWith(
+        caches.match(e.request).then((res) => res || fetch(e.request))
+    );
+});
